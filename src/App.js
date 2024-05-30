e5a9efccf87898a66ee7c15d5e126a96d0f135a8
Bharat <28brana@gmail.com>
2024-05-30 12:12:19 +0530

    Ui Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-
+import googleImg from './google.png'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='body'>
+        <div className='login-container'>
+          <p className='header'>Login to Dashboard</p>
+          <label>Email or Mobile Number</label>
+          <div>
+            <input type='text' className='input' />
+          </div>
+          <button className='primary-btn'>Next</button>
+          <div className='breaker-container'>
+            <div className='breaker' />
+            <span>or</span>
+            <div className='breaker' />
+          </div>
+          <div className='btn-container'>
+            <button className='google-btn'>
+              <img src={googleImg} alt='google-icon' />
+              Sign in with google
+            </button>
+          </div>
+        </div>
+        <p className='help'>Need help ? <span>Contact us </span></p>
+      </div>
     </div>
   );
 }
